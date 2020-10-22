d56bbdc99bf691c439014919e4246e750229a351
AndrosEt <1756238821@qq.com>
2020-10-22 14:24:44 +0800

    init commit

---
+++ src/main/kotlin/com/et/facerecognition/FacerecognitionApplication.kt
@@ -0,0 +1,11 @@
+package com.et.facerecognition
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FacerecognitionApplication
+
+fun main(args: Array<String>) {
+    runApplication<FacerecognitionApplication>(*args)
+}
